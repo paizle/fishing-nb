9b5754d213a04838f6c899991750d93332419e8b
matthew <matthewallanmacpherson@gmail.com>
2025-01-11 10:57:04 -0400

    Preparing deployment.

--- public/build/assets/Index-D3n8hYin.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-C34GtmeX.js";import{P as r}from"./PublicLayout-gzE67k46.js";function i(){return e.jsxs(r,{children:[e.jsx("header",{children:"* header *"}),e.jsx("main",{children:"* test *"})]})}export{i as default};
---
+++ public/build/assets/PublicLayout-De1aTIQd.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CZZnVWYO.js";function u({children:t}){return o.jsx("div",{className:"PublicLayout",children:t})}export{u as P};
