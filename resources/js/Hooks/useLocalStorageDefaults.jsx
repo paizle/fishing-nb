512556f4dfbfd5c19cfc3e0ebe629cce849b94ae
matthew <matthewallanmacpherson@gmail.com>
2025-01-14 11:13:43 -0400

    Added setting for gradient background.

---
+++ resources/js/Hooks/useLocalStorageDefaults.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+export default function useLocalStorageDefaults() {
+  const localStorage = useLocalStorage()
+
+  const defaults = {
+    settings: {
+      gradientBackground: true,
+    }
+  }
+
+  const { setItem, removeItem, clearStorage, storageChange } = localStorage
+
+  const getItem = (key) => {
+    if (defaults.hasOwnProperty(key)) {
+      return Object.assign(defaults[key], localStorage.getItem(key))
+    } else {
+      return localStorage.getItem(key)
+    }    
+  }
+
+  return { setItem, getItem, removeItem, clearStorage, storageChange };
+
+}
