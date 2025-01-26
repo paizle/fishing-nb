a60e06466ef4bb0786442bc52295e9e8b728ee55
matthew <matthewallanmacpherson@gmail.com>
2025-01-25 20:58:48 -0400

    Added cache policy.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -80,8 +80,8 @@ class ApiController extends Controller
 
     public function fishByLocation(Request $request, $location_id, $water_id = '0', $fish_id = '0')
     {
-        //$last_modified = $this->createLastModified(2025, 1, 20);
-        //$this->handleLastModifiedRequest($request, $last_modified);
+        $last_modified = $this->createLastModified(2025, 1, 20);
+        $this->handleLastModifiedRequest($request, $last_modified);
 
         $results_ids = [];
         $limits = FishLimit::query()->where('location_id', $location_id)->get();
@@ -133,7 +133,7 @@ class ApiController extends Controller
             ->toArray();
 
         return response(['limits' => $limits_by_water], 200)
-            ->header('Cache-Control', 'no-cache');
-            //->header('Last-Modified', $last_modified);
+            ->header('Cache-Control', 'no-cache')
+            ->header('Last-Modified', $last_modified);
     }
 }
