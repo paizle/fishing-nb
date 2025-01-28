cae26434ff16f6a722971af6c5cf6e2dc8231b4b
matthew <matthewallanmacpherson@gmail.com>
2025-01-28 12:04:04 -0400

    Preparing deployment.

---
+++ public/build/assets/useLandingPage-BISuTH3d.js
@@ -0,0 +1,1 @@
+import{r as a}from"./app-BCCgJSTG.js";import{u as o}from"./PublicLayout-BnsZs5HR.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
