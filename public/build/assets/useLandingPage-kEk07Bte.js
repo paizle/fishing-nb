b2e66dbc308231312f8450190d1d722ee4199884
matthew <matthewallanmacpherson@gmail.com>
2025-02-12 20:13:53 -0400

    Preparing deployment.

--- public/build/assets/useLandingPage-kEk07Bte.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./app-sb7yLzEq.js";import{u as o}from"./PublicLayout-taBMLHML.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
