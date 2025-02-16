e83b79842b0ac8283bad21aab4a0ab5159514da7
matthew <matthewallanmacpherson@gmail.com>
2025-02-16 14:42:28 -0400

    Preparing build.

---
+++ public/build/assets/useLandingPage-DNkGrQch.js
@@ -0,0 +1,1 @@
+import{r as a}from"./app-BlCin5dP.js";import{u as o}from"./PublicLayout-B2muosjr.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
