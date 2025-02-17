e555d1ee6dedc42b86f18adee93c7f3dfedae8cb
matthew <matthewallanmacpherson@gmail.com>
2025-02-17 09:46:03 -0400

    Preparing deplployment.

--- public/build/assets/useLandingPage-CybH85fk.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./app-CN6gpWes.js";import{u as o}from"./PublicLayout-BJT-L94N.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
---
+++ public/build/assets/useLandingPage-DViuTGIT.js
@@ -0,0 +1,1 @@
+import{r as a}from"./app-Bw70NceN.js";import{u as o}from"./PublicLayout-Dy39uGG-.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
