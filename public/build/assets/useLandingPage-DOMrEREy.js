69be1ab0a03b5b2ea229b75b482670eac95f45f3
matthew <matthewallanmacpherson@gmail.com>
2025-02-18 08:38:18 -0400

    Preparing deployment.

---
+++ public/build/assets/useLandingPage-DOMrEREy.js
@@ -0,0 +1,1 @@
+import{r as a}from"./app-DcVFl9p3.js";import{u as o}from"./PublicLayout-CiZx_dOm.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
--- public/build/assets/useLandingPage-DViuTGIT.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./app-Bw70NceN.js";import{u as o}from"./PublicLayout-Dy39uGG-.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
