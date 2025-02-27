fe4f6f49c7923ce09eb5592507f63889858ed421
matthew <matthewallanmacpherson@gmail.com>
2025-02-27 09:52:05 -0400

    Added react-scan to app.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,11 +1,17 @@
 import '../css/app.scss'
 import './bootstrap'
 
+import { scan } from "react-scan"; // must be imported before React and React DOM
+
 import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createRoot } from 'react-dom/client'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+
+scan({
+  enabled: true,
+})
 
 createInertiaApp({
 	title: (title) => `${appName}`,
