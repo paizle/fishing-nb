ea6bf282bf6e26e6b06def9d41703483b17a8402
matthew <matthewallanmacpherson@gmail.com>
2025-03-08 10:03:05 -0400

    Preparing deployment.

--- public/build/assets/useLandingPage-DOMrEREy.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./app-DcVFl9p3.js";import{u as o}from"./PublicLayout-CiZx_dOm.js";function g(t){const e=o();a.useEffect(()=>{e.set("settings",s=>s.landingPage=t)},[])}export{g as u};
