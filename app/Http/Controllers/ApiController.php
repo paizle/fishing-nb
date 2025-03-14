18c1f856432731a09688a1bae56bd7799bd93c3a
matthew <matthewallanmacpherson@gmail.com>
2025-03-13 21:09:53 -0300

    Moved business logic to service.

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Services\RestrictionsService;
 use App\Util\WatersCategoryName;
 use Exception;
 use Illuminate\Http\Request;
@@ -14,9 +15,12 @@ class ApiController extends Controller
 {
 	protected $fishService;
 
-	public function __construct(FishService $fishService)
+	protected $restrictionsService;
+
+	public function __construct(FishService $fishService, RestrictionsService $restrictionsService)
 	{
 		$this->fishService = $fishService;
+		$this->restrictionsService = $restrictionsService;
 	}
 
 	public function fishes(Request $request)
@@ -62,55 +66,22 @@ class ApiController extends Controller
 
 	public function fishByLocation(Request $request, $region_id, $water_id = '0', $fish_id = '0')
 	{
-		$results_ids = [];
-		$restrictions = FishingRestriction::query()->where('region_id', $region_id)->get();
 
-		$water_type = null;
-		if ($water_id !== '0') {
-			$water = Water::find($water_id);
-			$water_type = $this->getWaterType($water->name);
-			if ($water_type === null) {
-				throw new Exception('Water type not found.');
+		if ($water_id) {
+			if ($fish_id) {
+				$records = $this->restrictionsService->getByRegionFishAndWater($region_id, $fish_id, $water_id);
+			} else {
+				$records = $this->restrictionsService->getByRegionAndWater($region_id, $water_id);	
+			}
+		} else {
+			if ($fish_id) {
+				$records = $this->restrictionsService->getByRegionAndFish($region_id, $fish_id);
+			} else {
+				$records = $this->restrictionsService->getByRegion($region_id);	
 			}
 		}
 
-		foreach ($restrictions->toArray() as $restriction) {
-			$add = true;
-
-			if ($fish_id !== '0') {
-				if ($restriction['fish_id'] !== null && $restriction['fish_id'] != $fish_id) {
-					$add = false;
-				}
-			}
-
-			if ($water_id !== '0') {
-				if ($restriction['water_id'] === null) {
-					$test = true;
-					if (
-						$restriction['water_type'] !== null &&
-						$restriction['water_type'] !== $water_type->value
-					) {
-						$add = false;
-					}
-				} elseif ($restriction['water_id'] != $water_id) {
-					$add = false;
-				}
-			}
-
-			if ($add) {
-				$results_ids[] = $restriction['id'];
-			}
-		}
-
-		$restrictions_by_water = FishingRestriction::query()
-			->whereIn('id', $results_ids)
-			->with(['fish', 'water'])
-			->orderBy(Fish::select('name')->whereColumn('fish.id', 'fishing_restrictions.fish_id'))
-			->orderBy('season_start')
-			->get()
-			->toArray();
-
-		return response(['limits' => $restrictions_by_water]);
+		return response(['limits' => $records]);
 	}
 
 	protected function getWaterType($water_name)
