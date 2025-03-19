9d4f74120617b587bfc112065e8880b34e32d047
matthew <matthewallanmacpherson@gmail.com>
2025-03-19 17:35:00 -0300

    Preparing deployment.

--- public/build/assets/PublicLayout-B77Wcp63.js
+++
@@ -1,1 +0,0 @@
-import{c as a,j as s}from"./app-B8I1jQG5.js";function c({className:t="",children:n}){const o=a().getSettings();return s.jsx("div",{className:`PublicLayout ${t} ${o.gradientBackground?"gradient-background":""}`,children:n})}export{c as P};
