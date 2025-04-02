3fb7995723137f97efb2cbe01953c6bc6961f044
matthew <matthewallanmacpherson@gmail.com>
2025-04-02 16:39:49 -0300

    Updated axios config to send a timestamp as a querystring parameter to bust cache.

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,6 @@ import axios from 'axios'
 window.axios = axios
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.headers.common['X-TEST'] = 'test'
+window.axios.defaults.params = {}
+window.axios.defaults.params.nocache = Date.parse(import.meta.env.VITE_API_LAST_MODIFIED)
