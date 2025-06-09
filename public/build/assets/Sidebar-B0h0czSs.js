a8d4031bf71da99b5a5a3c5568398f40f3ded667
matthew <matthewallanmacpherson@gmail.com>
2025-06-09 11:09:08 -0300

    Allow build dir.

---
+++ public/build/assets/Sidebar-B0h0czSs.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-hti74d3x.js";function s({children:r}){return a.jsx("div",{className:"Sidebar",children:r})}export{s as default};
---
+++ public/build/assets/index-B2J4XDE4.js
@@ -0,0 +1,1 @@
+import{b as r}from"./app-hti74d3x.js";var a=r();export{a as r};
