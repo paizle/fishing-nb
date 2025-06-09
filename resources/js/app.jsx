8c23a350189b6400d6c340c53f14bb64bbeff709
matthew <matthewallanmacpherson@gmail.com>
2025-06-09 10:29:12 -0300

    Removed large files

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,18 +1,12 @@
 import '../css/app.scss'
 import './bootstrap'
 
-import { scan } from 'react-scan' // must be imported before React and React DOM
-
 import { createInertiaApp } from '@inertiajs/react'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createRoot } from 'react-dom/client'
 import { ApplicationContextProvider } from '@/Contexts/ApplicationContext'
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
-
-scan({
-	enabled: false,
-})
 
 createInertiaApp({
 	title: (title) => `${appName}`,
