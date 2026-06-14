f8a82cf7124e2c8e4987e2cc22864e20027798a2
matthew <matthewallanmacpherson@gmail.com>
2026-06-13 21:29:15 -0300

    Preparing deployment.

--- public/build/assets/Sidebar-DtV9GFVS.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./app-DIPW6cyQ.js";function s({children:r}){return a.jsx("div",{className:"Sidebar",children:r})}export{s as default};
--- public/build/assets/index-s7GIrXP7.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./app-DIPW6cyQ.js";var a=r();export{a as r};
